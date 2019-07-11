a1fb044d145f41f775e4140dab9d9ec7814e771f
Santa Claus <santa@northpole.com>
2019-07-11 09:29:44 +0300

    started project

---
+++ yearproj/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for yearproj project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yearproj.settings")
+
+application = get_wsgi_application()
