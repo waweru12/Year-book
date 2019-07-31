a1cdee6e984ba4454d96b06156a06b7650310f1d
Santa Claus <santa@northpole.com>
2019-07-31 10:22:32 +0300

    finished project

--- yearproj/settings.py
+++ yearproj/settings.py
@@ -78,11 +78,12 @@ WSGI_APPLICATION = 'yearproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'year',
+        'USER': 'austin',
+    'PASSWORD':'1234',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
